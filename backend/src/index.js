5567b91bbdeb417bf48ab9fd2540acb178b2a6b9
Deployer <deploy@local.com>
2026-05-05 00:11:55 +0500

    Fix express wildcard route

--- backend/src/index.js
+++ backend/src/index.js
@@ -41,7 +41,7 @@ const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
